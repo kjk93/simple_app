9d49823de5729d2743cf0adcce2f9969b746581f
Kyle Kravette <kkravette@gmail.com>
2013-12-30 14:40:41 -0500

    Start of sign in/out branch

--- db/schema.rb
+++ db/schema.rb
@@ -24,4 +24,6 @@ ActiveRecord::Schema.define(version: 20131228195908) do
     t.string   "password_digest"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
 end
