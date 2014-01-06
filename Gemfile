f3f26d8b32f6acb2ab7460c420b21338c6b8ec71
Kyle Kravette <kkravette@gmail.com>
2014-01-06 13:38:42 -0500

    Added admin attribute

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.2'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 group :development do
 # Use sqlite3 as the database for Active Record
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do 
 	factory :user do
-		name "Kyle Kravette"
-		email "kjk93@comcast.net"
+		sequence(:name) {|n| "Person #{n}"}
+		sequence(:email) {|n| "person_#{n}@example.com"}
 		password "america"
 		password_confirmation "america"
 	end
